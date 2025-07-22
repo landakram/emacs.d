0b0808254a0204ee6f0d5b9a584f121e0c98e5be
Mark Hudnall <mh@openai.com>
2025-07-22 13:10:49 -0700

    rust

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -2125,6 +2125,20 @@ See URL `https://beta.ruff.rs/docs/'."
             (lambda () (setq indent-tabs-mode nil)))
   (setq rust-format-on-save t))
 
+
+(with-eval-after-loads '(rust-mode lsp-mode)
+  (setq lsp-rust-analyzer-cargo-watch-command "clippy")
+  (setq lsp-inlay-hint-enable t)
+  (setq lsp-eldoc-render-all nil)
+
+  (setq lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (setq lsp-rust-analyzer-display-chaining-hints t)
+  (setq lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names t)
+  (setq lsp-rust-analyzer-display-closure-return-type-hints t)
+  (setq lsp-rust-analyzer-display-parameter-hints t)
+  (setq lsp-rust-analyzer-display-reborrow-hints nil)
+  (lsp-inlay-hints-mode t))
+
 (use-package vimrc-mode
   :ensure t
   :init
